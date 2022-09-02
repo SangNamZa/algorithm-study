0ac89c669766248865c50c2840ac200d267d6446
MinJunKim21 <101058125+MinJunKim21@users.noreply.github.com>
2022-09-02 21:19:34 +0900

    Create findKim.js

---
+++ MinJunKim/findKim.js
@@ -0,0 +1,11 @@
+function solution(seoul) {
+  let answer = '';
+  let place = 0;
+  for (let i = 0; i < seoul.length; i++) {
+    if ((seoul[i] === 'Kim')) {
+      place = i;
+    }
+  }
+  answer = '김서방은 ' + place + '에 있다';
+  return answer;
+}
