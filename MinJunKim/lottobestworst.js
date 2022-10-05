2d21cd18fc4142a1acdb5177b0ffe2fca99b3d8d
MinJunKim21 <101058125+MinJunKim21@users.noreply.github.com>
2022-10-05 20:16:27 +0900

    Update lottobestworst.js

--- MinJunKim/lottobestworst.js
+++ MinJunKim/lottobestworst.js
@@ -1,74 +1,23 @@
 function solution(lottos, win_nums) {
-        let a = 0;
-        let b = 0;
-        let c = 0;
-        let rankA = 6;
-        let rankB = 6;
-
-
-        win_nums.map((winnum) => {
-          if (lottos.includes(winnum)) {
-            a += 1;
-          }
-        });
-
-        for (const obj of lottos) {
-          if (obj === 0) {
-            c += 1;
-          }
-        }
-
-        if (a + c === 6) {
-          rankA = 1;
-        }
-        if (a + c === 5) {
-          rankA = 2;
-        }
-        if (a + c === 4) {
-          rankA = 3;
-        }
-        if (a + c === 3) {
-          rankA = 4;
-        }
-        if (a + c === 2) {
-          rankA = 5;
-        }
-        if (a + c === 1) {
-          rankA = 6;
-        }
-        if (a + c === 0) {
-          rankA = 6;
-        }
-
-        if (a === 6) {
-          rankB = 1;
-        }
-
-        if (a === 5) {
-          rankB = 2;
-        }
-
-        if (a === 4) {
-          rankB = 3;
-        }
-
-        if (a === 3) {
-          rankB = 4;
-        }
-
-        if (a === 2) {
-          rankB = 5;
-        }
-
-        if (a === 1) {
-          rankB = 6;
-        }
-
-        if (a === 0) {
-          rankB = 6;
-        }
-
-        var answer = [rankA, rankB];
-
-        return answer;
-      }
+    var answer = [];
+    let maxScore = 0
+    let zeroNum = 0
+    let minScore = 0
+    for(let i = 0; i < lottos.length; i++){
+        if(win_nums.includes(lottos[i])){minScore++}
+        if(lottos[i]===0){zeroNum++}
+    }
+    maxScore = minScore + zeroNum
+    console.log(minScore)
+    function rank(n){
+        if(n===6){return 1}
+        if(n===5){return 2}
+        if(n===4){return 3}
+        if(n===3){return 4}
+        if(n===2){return 5}
+        else {return 6}
+    }
+    answer.push(rank(maxScore))
+    answer.push(rank(minScore))
+    return answer;
+}
