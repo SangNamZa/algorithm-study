899d2bb2686c6e553f7ca9b3b40bb1e3080ac068
MinJunKim21 <101058125+MinJunKim21@users.noreply.github.com>
2022-11-25 17:16:41 +0900

    Create slicearray.js

---
+++ MinJunKim/slicearray.js
@@ -0,0 +1,13 @@
+function solution(n, left, right) {
+    // 1234 2234 3334 4444
+    // 123 223 333 
+    // 답 보고서 이해함. 좌표로 해서 (a,b) a,b중에서 큰수 +1 하면됨
+    // a,b좌표로 바꾸는법은 n으로 나눈 몫과 나머지로 설정
+//    const ans = [];
+
+//     while (left <= right) {
+//         ans.push(Math.max(Math.floor(left / n), left++ % n) + 1);
+//     }
+
+//     return ans;
+}
