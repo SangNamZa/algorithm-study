505b87e2a6e787735fa328427e09b96be779d8c7
MinJunKim21 <101058125+MinJunKim21@users.noreply.github.com>
2023-01-03 01:04:56 +0900

    Create eatddang.js

---
+++ MinJunKim/eatddang.js
@@ -0,0 +1,12 @@
+function solution(land) {
+    var answer = 0;
+    let ansArr = []
+    //BFS문제인가... 전체다 구해서 비교하면 되는가...
+    for(let i = 0; i<land.length; i++){
+    let Max = Math.max(land[i][0],land[i][1],land[i][2],land[i][3])
+    console.log(Max)
+
+    }
+
+    return answer;
+}
