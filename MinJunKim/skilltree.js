7e10ddbc66e5771856d03832eba4c1f2dd4d552c
MinJunKim21 <101058125+MinJunKim21@users.noreply.github.com>
2023-01-02 18:36:04 +0900

    Update skilltree.js

--- MinJunKim/skilltree.js
+++ MinJunKim/skilltree.js
@@ -1,18 +1,19 @@
 function solution(skill, skill_trees) {
     var answer = 0;
-    // C의 위치값 구하고, temp값을 다음꺼 업뎃, 비교, 없으면 통과하려햇음
+    // C의 위치값 구하고, temp값을 다음꺼 업뎃, 비교, 없으면 통과
     let arr = []
     for(let i = 0; i<skill_trees.length; i++){
         for(let j = 0; j<skill.length;j++){
-            arr.push(skill_trees[i].indexOf(skill[j]))
+            arr.push(skill_trees[i].indexOf(skill[j])===-1 ? 100 : skill_trees[i].indexOf(skill[j]))
+            //스킬트리에 사용이 안되었을경우 100으로 취급함
         }
-        // 각 인덱스값들로 배열을 만들었고, 없는값은 -1로 인덱스값 처리됨. 근데 조건 중에서 먼저 선행해야만 하는 경우를 빼먹고 진행하였음
         let filterArr = arr.filter(num =>num!==-1)
-        let sortArr = arr.filter(num =>num!==-1).sort()
-        console.log(arr)
-        console.log(filterArr)
-        console.log(sortArr)
-        if(filterArr == sortArr){console.log('answer')}
+        let sortArr = arr.filter(num =>num!==-1).sort((a,b)=>{return a-b})
+        console.log(JSON.stringify(filterArr))
+        console.log(JSON.stringify(sortArr))
+        //JSON.stringify로 배열값이 같은지 비교 가능하게함
+        
+        if(JSON.stringify(filterArr)===JSON.stringify(sortArr)){answer++}
 
         arr = []
         console.log(arr)
